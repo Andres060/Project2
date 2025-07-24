a672aef8fe6427dfc1af58e73d0aa4694ca8bed6
quits666 <simonurrego786@gmail.com>
2025-07-24 14:04:33 -0500

    Update Administrador.py
    Correcion de Simon

--- Administrador.py
+++ Administrador.py
@@ -1,20 +1,4 @@
 from Usuario import Usuario
 
 class Administrador(Usuario):
-    def __init__(self):
-        pass
-
-    def agregarVuelo(self, vuelo):
-        pass
-
-    def modificarVuelo(self, codigo):
-        pass
-
-    def consultarVuelo(self, Vuelo):
-        pass
-
-    def verPasajeros(self, codigo):
-        pass
-
-    def toString(self):
         pass
