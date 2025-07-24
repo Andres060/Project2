ab3a159511234db873af8d25f87edbb61aad9103
quits666 <simonurrego786@gmail.com>
2025-07-24 14:08:26 -0500

    Update Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -1,22 +1,22 @@
 from abc import ABC, abstractmethod
+import os
+import tkinter as tk
+from tkinter import messagebox
 
 class Usuario(ABC):
-    def __init__(self, nombre, correo, id, contraseña):
+    def __init__(self, nombre, correo, id, contrasena):
         self.__nombre = nombre
         self.__correo = correo
         self.__id = id
-        self.__contraseña = contraseña
+        self.__contrasena = contrasena
 
     @abstractmethod
-    def iniciarSesion(self, nombre, contrasena,):
-        pass
+    def cambiarContraseña(self, nueva):    
+        try:
+            self.__contrasena = nueva
+        except Exception:
+            self._mostrar_error()
 
-    @abstractmethod
-    def cambiarContraseña(self, contraseña):
-        pass
-
-    @abstractmethod
-    def toString(self):
-        pass
-
-        
+    def _mostrar_error(self):
+        if 'tk' in globals():
+            messagebox.showerror("Error","Ha ocurrido un error, intentalo nuevamente")
