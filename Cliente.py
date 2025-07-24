b6a92f617ba177f46219cd507a66888787804d6f
quits666 <simonurrego786@gmail.com>
2025-07-24 15:45:34 -0500

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,6 +1,27 @@
-from Usuario import Usuario
+from abc import ABC, abstractmethod
 
-class Cliente(Usuario):
-    def __init__(self,nombre, correo, id, contrasena, millas=0):
-        super().__init__(nombre, correo, id, contrasena)
-        self.__millas = millas
+class Usuario(ABC):
+    def _init_(self, nombre, correo, id, contrasena):
+        self.__nombre = nombre
+        self.__correo = correo
+        self.__id = id
+        self.__contrasena = contrasena
+
+    def get_id(self):
+        return self.__id
+
+    def get_nombre(self):
+        return self.__nombre
+        
+    def get_correo(self):
+        return self.__correo
+
+    def get_contrasena(self):
+        return self.__contrasena
+
+    @abstractmethod
+    def cambiarContraseña(self, nueva):
+        pass
+
+    def verificar_contrasena(self, contrasena_ingresada):
+        return self.__contrasena == contrasena_ingresada
