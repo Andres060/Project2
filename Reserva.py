7cb4f88889772f52a05e7f190a79b6f2dff3755f
quits666 <simonurrego786@gmail.com>
2025-07-24 14:06:57 -0500

    Update Reserva.py

--- Reserva.py
+++ Reserva.py
@@ -1,25 +1,40 @@
-class Reserva():
-    def __init__(self, numReserva, cliente, vuelo, pasajeros, total, checkInRealizado, millasAcumuladas, equipaje):
-        self.__numReserva = numReserva
-        self.__cliente = cliente
-        self.__vuelo = vuelo
-        self.__pasajeros = pasajeros
-        self.__total = total
-        self.__checkInRealizado = checkInRealizado
-        self.__millasAcumuladas = millasAcumuladas
-        self.__equipaje = equipaje
+from Equipaje import Equipaje
+from Pasajero import Pasajero
+from typing import List, Dict
+import os
+import tkinter as tk
+from tkinter import messagebox
 
-    def calcularTotal(self):
-        pass
 
-    def modificar(self):
-        pass
+class Reserva:
+    id_counter = 1
 
-    def cancelar(self):
-        pass
+    def __init__(self, numero_reserva: str, cliente_doc: str, vuelo_cod: str, pasajeros: List['Pasajero']):
+        self._numero_reserva = numero_reserva
+        self._cliente_doc = cliente_doc
+        self._vuelo_cod = vuelo_cod
+        self._pasajeros = pasajeros
+        self._check_in_realizado = False
+        self._equipaje: List[Equipaje] = []
+        self._millas_acumuladas = 0
+        self._total = self.calcular_total()
 
-    def realizarCheckIn(self):
-        pass
+    def calcular_total(self) -> float:
+        try:
+            return len(self._pasajeros) * 235000
+        except Exception:
+            self._mostrar_error()
+            return 0.0
 
-    def toString(self):
-        pass
+    def realizar_check_in(self):
+        try:
+            self._check_in_realizado = True
+            self._millas_acumuladas = 500
+        except Exception:
+            self._mostrar_error()
+
+    def _mostrar_error(self):
+        if 'tk' in globals():
+            messagebox.showerror("Error", "Ha ocurrido un error, lo sentimos")
+        else:
+            print("Ha ocurrido un error, lo sentimos")
